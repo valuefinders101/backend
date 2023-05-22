409f180269832025dcc6903d922f349969c68b70
Uhembenelson <uhembenelson@gmail.com>
2023-05-22 16:38:05 +0100

    new update from nelson

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,4 @@ import { Login } from "../controllers/User/Login.js";
 router.post("/api/v1/register", Signup);
 //login user
 router.post("/api/v1/login", Login);
-
-
 export { router as userRoute };
