a610c5b78dcbc1f21b03c5cae4298528c7e068f6
Uhembenelson <uhembenelson@gmail.com>
2023-07-11 17:02:30 +0100

    created update submit application endpoint

--- controllers/Post/Index.js
+++ controllers/Post/Index.js
@@ -29,4 +29,33 @@ export const getPost = async (req, res, next)=>{
     return res.status(200).json({msg: "All Post", data: allPost}) 
 }
 
+export const updatePost = async(req, res) => {
+    const id = req.body.id
+    const user_id = req.user._id; 
+    const findPost = await Post.findOne({ _id: id }); 
+    const newContact = {
+        user_id: req.user._id,  
+        firstName: req.user.firstName, 
+        lastName: req.user.lastName
+       }
+
+      
+
+    if(findPost){
+       const newme = findPost.numberOfApplicants
+       let check = newme.find(user => user.user_id == user_id)
+       if(check === undefined){
+        newme.push(newContact)
+        findPost.save()
+        res.status(201).send({ msg: "Application Submitted"})
+       }else{
+        res.status(201).send({msg : "You have applied for this Job already"})
+       }
+ 
+    }else{
+       res.status(403).json('post not found')
+    }
+
+    }
+
 
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,11 +1,12 @@
 import  express from "express";
 const router = express.Router();
 import { ValidateToken } from "../middleware/ValidateToken.js";
-import { createPost, getPost } from "../controllers/Post/Index.js";
+import { createPost, getPost, updatePost } from "../controllers/Post/Index.js";
 
 
 //create user
 router.post("/api/v1/create-post", ValidateToken, createPost);
 router.get("/api/v1/get-all-post", ValidateToken, getPost)
+router.post("/api/v1/update-post", ValidateToken, updatePost);
 
 export { router as postRoute };
