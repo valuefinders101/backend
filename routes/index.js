9544046bc8a0bea08956328f6af0303fa71a668c
Uhembenelson <uhembenelson@gmail.com>
2023-10-25 17:59:25 +0100

    another update

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ export * from './userRoute.js';
 export * from './postRoute.js';
 export * from './ChatRoomRoute.js'; 
 export * from './chatMessages.js' ; 
-export * from './Socket.Route.js'; 
 export * from './contractRoute.js'; 
 
