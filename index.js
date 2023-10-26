b403df41b9d9180833f610c9470d7a3379c8c1d6
Uhembenelson <uhembenelson@gmail.com>
2023-10-26 14:47:03 +0100

    new update 15

--- index.js
+++ index.js
@@ -6,31 +6,14 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
  
-import { createServer } from 'http';
-
-
-// import { postMessage } from './controllers/User/ChatRoom.js';
-// import { ChatMessages } from './models/User/chatmessages.js';
-
-
-
 const port = 3000 
 const app = express();
-const httpServer = createServer(app)
-
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.set('view engine', 'ejs'); //view engine others are hbs, html or react  
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: false,
-//     method: ['GET', 'POST', 'PUT', 'DELETE'],
 
-// }));
-//app.use(crossOrigin());
-//app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from
@@ -39,14 +22,10 @@ app.use(function(req, res, next) {
 });
 
 //import routes
-import {userRoute, postRoute, contractRoute} from './routes/index.js';
-
-
+import { userRoute, postRoute, contractRoute } from './routes/index.js';
 //routes
-
 app.use('/user', userRoute);
-app.use('/post', postRoute); 
-// app.use('/room', roomRoute);  
+app.use('/post', postRoute);  
 app.use('/contract', contractRoute);
 
 
@@ -55,17 +34,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to value finders, find jobs here ')
 })
 
-
-
-//connect to mongodb
-// mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('Connected to MongoDB...'))
-//     .catch(err => console.error('Could not connect to MongoDB...', err));
-
-// httpServer.listen(process.env.PORT || port, () => {
-//     console.log(`listening at http://localhost:${port}`)
-//     }
-// )
 
 //connect to mongodb
 mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 export * from './userRoute.js';
 export * from './postRoute.js';
-// export * from './ChatRoomRoute.js'; 
-export * from './chatMessages.js' ; 
 export * from './contractRoute.js'; 
 
