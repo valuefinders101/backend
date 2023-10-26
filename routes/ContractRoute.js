79880ba1d5da35c1e4593578bf904a6a71479471
Uhembenelson <uhembenelson@gmail.com>
2023-10-26 14:26:03 +0100

    new update 10

--- routes/ContractRoute.js
+++ routes/ContractRoute.js
@@ -5,11 +5,11 @@ import { createContract, getMycontracts, updateContract, waitingApproval, comple
 import { ValidateToken } from "../middleware/ValidateToken.js";
 
 // router.post("/api/v1/sendContractRequest", ValidateToken, createContract);
-router.get("/api/v1/allcontracts", ValidateToken, getMycontracts)
-router.post("/api/v1/updateContract", ValidateToken, updateContract);
-router.post("/api/v1/approvalRequest", ValidateToken, waitingApproval);
-router.post("/api/v1/completed", ValidateToken, completed);
-router.get("/api/v1/banklist", ValidateToken, Banklist)
+// router.get("/api/v1/allcontracts", ValidateToken, getMycontracts)
+// router.post("/api/v1/updateContract", ValidateToken, updateContract);
+// router.post("/api/v1/approvalRequest", ValidateToken, waitingApproval);
+// router.post("/api/v1/completed", ValidateToken, completed);
+// router.get("/api/v1/banklist", ValidateToken, Banklist)
 
 export { router as contractRoute };
 
