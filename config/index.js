08829201fef718c9032d031b0873715490220dfd
Uhembenelson <uhembenelson@gmail.com>
2023-10-26 15:19:22 +0100

    new update 23

--- config/index.js
+++ config/index.js
@@ -1,4 +1,9 @@
 
-export const  MONGO_URL = "mongodb://localhost/valuefinder";
+//export const  MONGO_URL = "mongodb://localhost/valuefinder";
 export const api_key ='251be472'
+export const MONGO_URL = "mongodb+srv://valuefinders101:Mayorgnn088@cluster0.nanvix6.mongodb.net/?retryWrites=true&w=majority"
+//mongodb+srv://valuefinders101:Mayorgnn088@cluster0.nanvix6.mongodb.net/?retryWrites=true&w=majority
+
+
+
 //export const MONGO_URL = "mongodb+srv://kudina:mayorgnn088@cluster0.0pqswor.mongodb.net/?retryWrites=true&w=majority"
--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 //connect to mongodb
 mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.listen(port, () => {
     console.log(`Valuefinders listening at http://localhost:${port}`)
