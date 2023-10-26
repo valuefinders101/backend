0d1500151c265832cce24f4cb3da87893fe9b174
Uhembenelson <uhembenelson@gmail.com>
2023-10-26 14:20:14 +0100

    new update 8

--- routes/ContractRoute.js
+++ routes/ContractRoute.js
@@ -1,15 +1,15 @@
-// import  express from "express";
-// const router = express.Router();
+import  express from "express";
+const router = express.Router();
 
-// import { createContract, getMycontracts, updateContract, waitingApproval, completed, Banklist} from "../controllers/Contract/Index.js";
-// import { ValidateToken } from "../middleware/ValidateToken.js";
+import { createContract, getMycontracts, updateContract, waitingApproval, completed, Banklist} from "../controllers/Contract/Index.js";
+import { ValidateToken } from "../middleware/ValidateToken.js";
 
-// router.post("/api/v1/sendContractRequest", ValidateToken, createContract);
-// router.get("/api/v1/allcontracts", ValidateToken, getMycontracts)
-// router.post("/api/v1/updateContract", ValidateToken, updateContract);
-// router.post("/api/v1/approvalRequest", ValidateToken, waitingApproval);
-// router.post("/api/v1/completed", ValidateToken, completed);
-// router.get("/api/v1/banklist", ValidateToken, Banklist)
+router.post("/api/v1/sendContractRequest", ValidateToken, createContract);
+router.get("/api/v1/allcontracts", ValidateToken, getMycontracts)
+router.post("/api/v1/updateContract", ValidateToken, updateContract);
+router.post("/api/v1/approvalRequest", ValidateToken, waitingApproval);
+router.post("/api/v1/completed", ValidateToken, completed);
+router.get("/api/v1/banklist", ValidateToken, Banklist)
 
-// export { router as contractRoute };
+export { router as contractRoute };
 
